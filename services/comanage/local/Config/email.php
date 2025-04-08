8f094af27e2d31392d3875e9459d9abbe092483e
Mike Thicke <mikethicke@gmail.com>
2025-04-08 16:11:54 -0400

    COmanage email configuration using environment variables

--- services/comanage/local/Config/email.php
+++ services/comanage/local/Config/email.php
@@ -1,9 +1,21 @@
 <?php
 
 class EmailConfig {
-
-  public $default = array(
-		'from' => 'you@localhost',
+	public array $default;
+	public function __construct() {
+		$this->default = array(
+			'transport' => 'Smtp',
+			'from' => [getenv('COMANAGE_EMAIL_FROM'), 'Knowledge Commons'],
+			'host' => getenv('COMANAGE_EMAIL_HOST'),
+			'port' => getenv('COMANAGE_EMAIL_PORT'),
+			'timeout' => 30,
+			'username' => getenv('COMANAGE_EMAIL_USERNAME'),
+			'password' => getenv('COMANAGE_EMAIL_PASSWORD'),
+			'client' => null,
+			'log' => false,
+			'charset' => 'utf-8',
+			'headerCharset' => 'utf-8',
 		);
+	}
 
 }
